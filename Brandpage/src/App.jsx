cbe128f5d64f01f7054a3f48c52d3b468bde6938
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-15 09:54:36 +0530

    Brand page

---
+++ Brandpage/src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+    return(
+        <>
+        <div>
+            <nav>
+                <ul>
+                    <li href="#"></li>
+                </ul>
+            </nav>
+        </div>
+        </>
+    )
+}
+export default App;
