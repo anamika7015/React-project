863ac43e8e6cc51d0d7253b17f79467ce8e7a2c3
anamika7015 <149082695+anamika7015@users.noreply.github.com>
2024-06-15 11:49:32 +0530

    brand page shoes

--- Brandpage/src/App.jsx
+++ Brandpage/src/App.jsx
@@ -2,10 +2,18 @@ function App(){
     return(
         <>
         <div>
-            <nav>
+            <nav className="container">
+                <div className="logo">
+                    <img src="image1.jpg" alt="logo" />
+                </div>
                 <ul>
-                    <li href="#"></li>
+                    <li href="#">Menu</li>
+                    <li href="#">Location</li>
+                    <li href="#">About</li>
+                    <li href="#">Contact</li>
+
                 </ul>
+                <button>login</button>
             </nav>
         </div>
         </>
---
+++ Brandpage/src/Hero.jsx
@@ -0,0 +1,31 @@
+function HeroSection() {
+  return (
+    <>
+      <main className="hero container">
+        <div className="hero-content">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </p>
+          <div className="hero-btn">
+            <button>Shop Now</button>
+            <button className="btn2">Category</button>
+          </div>
+          <div className="shopping">
+            <p>Also Available On</p>
+          </div>
+          <div className="brand-icon">
+            <img src="image2.jpg" alt="" />
+            <img src="image3.jpg" alt="" />
+          </div>
+        </div>
+        <div className="hero-images">
+            <img src="image4.jpg" alt="hero-image" />
+        </div>
+      </main>
+    </>
+  );
+}
+export default HeroSection;
