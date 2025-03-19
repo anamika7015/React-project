922845f12068c5b915d7efd2bee186dc71b404e6
Anamika Pandey <149082695+anamika7015@users.noreply.github.com>
2025-03-19 22:55:13 +0530

    day4

---
+++ 02reactproject/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './component/Navbar'
+import Header from './component/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar  text="name is namika" string="jasdfhda"/>
+      <Header text = "radhe radhe" />
+      
+    </div>
+  )
+}
+
+export default App
+
---
+++ 02reactproject/src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <div>
+      header {props.text}
+      <button>ansfhsa</button>
+    </div>
+  )
+}
+
+export default Header
